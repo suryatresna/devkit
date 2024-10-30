4571de862f63536ecf7d367e0228a3e06c892c3d
Surya Tresna <surya.tresna@mekari.com>
2024-10-30 21:25:03 +0700

    add new command

---
+++ cmd/kafka/consume.go
@@ -0,0 +1,107 @@
+/*
+Copyright © 2024 Surya Tresna <surya.tresna@gmail.com>
+*/
+package kafka
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/twmb/franz-go/pkg/kadm"
+	"github.com/twmb/franz-go/pkg/kgo"
+)
+
+// consumeCmd represents the consumer command
+var consumeCmd = &cobra.Command{
+	Use:   "consumer",
+	Short: "consume manual message commit",
+	Long: `Consumer message to kafka via command
+	`,
+	Run: consumeMessage,
+}
+
+func init() {
+	KafkaCmd.AddCommand(consumeCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// consumeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// consumeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	consumeCmd.Flags().StringP("group", "g", "", "consumer group")
+	consumeCmd.Flags().StringP("topic", "t", "", "topic name")
+	consumeCmd.Flags().StringP("brokers", "b", "", "brokers")
+
+}
+
+func consumeMessage(cmd *cobra.Command, args []string) {
+	start := time.Now()
+	defer func() {
+		fmt.Println("elapsed time:", time.Since(start))
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		os.Exit(1)
+		fmt.Println("interrupted, elapsed time: ", time.Since(start))
+	}()
+
+	brokers := cmd.Flag("brokers").Value.String()
+	if brokers == "" {
+		fmt.Println("brokers is required")
+		return
+	}
+
+	group := cmd.Flag("group").Value.String()
+
+	topic := cmd.Flag("topic").Value.String()
+
+	seeds := kgo.SeedBrokers(strings.Split(brokers, ",")...)
+
+	var adm *kadm.Client
+	{
+		cl, err := kgo.NewClient(seeds)
+		if err != nil {
+			fmt.Println("failed to create client")
+			return
+		}
+		adm = kadm.NewClient(cl)
+	}
+
+	os, err := adm.FetchOffsetsForTopics(context.Background(), group, topic)
+	if err != nil {
+		fmt.Println("failed to fetch offsets")
+		return
+	}
+
+	cl, err := kgo.NewClient(seeds, kgo.ConsumePartitions(os.KOffsets()))
+	if err != nil {
+		fmt.Println("failed to create client")
+		return
+	}
+	defer cl.Close()
+
+	fmt.Println("Waiting for one record...")
+	fs := cl.PollRecords(context.Background(), 1)
+
+	if err := adm.CommitAllOffsets(context.Background(), group, kadm.OffsetsFromFetches(fs)); err != nil {
+		fmt.Println("failed to commit offsets")
+		return
+	}
+
+	r := fs.Records()[0]
+	fmt.Printf("Successfully committed record on partition %d at offset %d!\n", r.Partition, r.Offset)
+
+}
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/lib/pq v1.10.9
 	github.com/spf13/cobra v1.8.1
 	github.com/twmb/franz-go v1.18.0
+	github.com/twmb/franz-go/pkg/kadm v1.14.0
 )
 
 require (
@@ -22,7 +23,7 @@ require (
 	github.com/fatih/color v1.12.0 // indirect
 	github.com/flynn-archive/go-shlex v0.0.0-20150515145356-3f9db97f8568 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
@@ -32,5 +33,6 @@ require (
 	github.com/robfig/cron v1.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/twmb/franz-go/pkg/kmsg v1.9.0 // indirect
-	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
 )
